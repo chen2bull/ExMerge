61649c66d3263187f6df9ce7a17016344419a0a3
chenmingjian <cmingjian@qq.com>
2019-02-14 23:16:30 +0800

    合并

--- src/main/java/exMerge/XlsMerger.java
+++ src/main/java/exMerge/XlsMerger.java
@@ -6,6 +6,9 @@ import name.fraser.neil.plaintext.diff_match_patch;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class XlsMerger {
@@ -21,20 +24,47 @@ public class XlsMerger {
         String baseText = new Xls2JsonParser(base, true).toJsonString();
         String theirsText = new Xls2JsonParser(theirs, true).toJsonString();
         String mineText = new Xls2JsonParser(mine, true).toJsonString();
+        try {
+            LinkedList<diff_match_patch.Patch> minePatchs = dmp.patch_make(baseText, mineText);
+            LinkedList<diff_match_patch.Patch> theirsPatchs = dmp.patch_make(baseText, theirsText);
 
-        LinkedList<diff_match_patch.Patch> minePatchs = dmp.patch_make(baseText, mineText);
-        Object[] results = dmp.patch_apply(minePatchs, theirsText);
-        String resultText = (String) results[0];
-
-        try{
+            System.out.println(minePatchs);
+            System.out.println(theirsPatchs);
+            Object[] results = dmp.patch_apply(minePatchs, baseText);
+            String resultText = (String) results[0];
+            boolean[] resultBool = (boolean[]) results[1];
+            System.out.println(Arrays.toString(resultBool));
+            results = dmp.patch_apply(theirsPatchs, resultText);
+            resultText = (String) results[0];
+            outputTextFile(merged+"_step1", resultText);
+            resultBool = (boolean[]) results[1];
+            System.out.println(Arrays.toString(resultBool));
             Json2XlsParser jParser = new Json2XlsParser(resultText);
             Workbook wb = jParser.toExcel();
             FileOutputStream fout = new FileOutputStream(merged);
             wb.write(fout);
             fout.close();
             wb.close();
+            outputTextFile(merged+"_step2", resultText);
         } catch (Exception e) {
             throw new Exception("自动合并失败,请手动合并", e);
+        } finally {
+            outputTextFile(base, baseText);
+            outputTextFile(theirs, theirsText);
+            outputTextFile(mine, mineText);
         }
     }
+
+    public static boolean outputTempFile = true;
+
+    private static void outputTextFile(String merged, String resultText) throws IOException {
+        if (!outputTempFile) {
+            return;
+        }
+        String mergedTextFile = merged + ".json";
+        FileWriter fileWriter = new FileWriter(mergedTextFile);
+        fileWriter.write(resultText);
+        fileWriter.close();
+    }
+
 }
---
+++ src/test/java/exMerge/XlsMergerTest.java
@@ -0,0 +1,35 @@
+package exMerge;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class XlsMergerTest {
+
+    @Test
+    public void mainTest() throws Exception {
+        String[] files = {"p抽取base.xls", "p抽取theirs.xls", "p抽取mine.xls", "p抽取merged.xls"};
+        String[] fileFullPaths = new String[4];
+        for (int i = 0; i < files.length; i++) {
+            fileFullPaths[i] = String.join(File.separator, "src", "test", "resources", files[i]);
+        }
+        System.out.println(Arrays.toString(fileFullPaths));
+        XlsMerger.outputTempFile = true;
+        XlsMerger.main(fileFullPaths);
+    }
+
+    @Test
+    public void mainTest2() throws Exception {
+        String[] files = {"f副本奖励base.xls", "f副本奖励theirs.xls", "f副本奖励mine.xls", "f副本奖励merged.xls"};
+        String[] fileFullPaths = new String[4];
+        for (int i = 0; i < files.length; i++) {
+            fileFullPaths[i] = String.join(File.separator, "src", "test", "resources", files[i]);
+        }
+        System.out.println(Arrays.toString(fileFullPaths));
+        XlsMerger.outputTempFile = true;
+        XlsMerger.main(fileFullPaths);
+    }
+}
